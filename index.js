34b1802c11251ee0b5eca322d157b22a2e779a91
Tai Cristiano <taductaik56a3@gmail.com>
2019-03-04 15:27:00 +0700

    Handling event

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import HandlingInput from './HandlingInput';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => HandlingInput);
