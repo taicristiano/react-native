041f666ea5f89174276ead8dcd45f20f6131133a
Tai Cristiano <taductaik56a3@gmail.com>
2019-03-04 14:19:38 +0700

    Flex

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import FixedDimensionsBasics from './FixedDimensionsBasics';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => FixedDimensionsBasics);
